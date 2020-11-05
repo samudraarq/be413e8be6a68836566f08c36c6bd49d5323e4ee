a3e984269055080f93491c472d7e555030e251f6
Samudra Faris Arqam <samudrafaris@gmail.com>
2020-11-05 10:06:52 +0700

    Installing date-fns lib

--- src/components/TopBar/DatePicker/DatePicker.js
+++ src/components/TopBar/DatePicker/DatePicker.js
@@ -1,7 +1,11 @@
 import styled from "styled-components";
 
+const Wrapper = styled.div`
+  border-bottom: 1px solid #f1f1f2;
+`;
+
 const DatePicker = () => {
-  return <div>From date picker</div>;
+  return <Wrapper>From date picker</Wrapper>;
 };
 
 export default DatePicker;
