31c1d1ed3c628a7257b5984c0898c84936221bf7
Samudra Faris Arqam <samudrafaris@gmail.com>
2020-11-05 09:59:06 +0700

    DatePicker: Initialized date picker

---
+++ src/components/TopBar/DatePicker/DatePicker.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const DatePicker = () => {
+  return <div>From date picker</div>;
+};
+
+export default DatePicker;
--- src/components/TopBar/TopBar.js
+++ src/components/TopBar/TopBar.js
@@ -1,9 +1,11 @@
+import DatePicker from "./DatePicker/DatePicker";
 import Location from "./Location/Location";
 
 const TopBar = () => {
   return (
     <>
       <Location />
+      <DatePicker />
     </>
   );
 };
